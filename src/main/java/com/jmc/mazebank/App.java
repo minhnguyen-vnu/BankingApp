986a35cf3d071b08a63b1eaa2e3dece7df27d5c0
Minh Nguyen <23020114@vnu.edu.vn>
2024-09-08 17:40:07 +0700

    push all

---
+++ src/main/java/com/jmc/mazebank/App.java
@@ -0,0 +1,17 @@
+package com.jmc.mazebank;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Etihad_Bank");
+        stage.show();
+    }
+}
